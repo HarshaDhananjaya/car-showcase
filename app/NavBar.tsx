3cb9d442c640a0ee13a598a75cdce59975022d31
Harsha Liyanage <harsha@mediwave.io>
2025-03-05 12:37:34 +0530

    Add NavBar component and integrate it into the layout

---
+++ app/NavBar.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { FcWorkflow } from "react-icons/fc";
+
+const NavBar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
+
+  return (
+    <nav className="flex space-x-5 border-b border-gray-500 mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FcWorkflow size={30} />
+      </Link>
+      <ul className="flex space-x-5">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className="text-zinc-500 hover:text-zinc-800"
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
